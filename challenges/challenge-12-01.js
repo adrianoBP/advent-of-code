02d6035827e30e90bbda66be1ef6762fb657c329
adrianoBP <adriano.boccardo@gmail.com>
2022-11-30 11:56:15 +0000

    Update readme

--- challenges/challenge-12-01.js
+++ challenges/challenge-12-01.js
@@ -1,9 +1,11 @@
+// Example!
 export const partOne = (rows) => {
   return rows.map((row) =>
     parseInt(row, 10),
   ).reduce((acc, curr) => acc + curr, 0);
 };
 
+// Example!
 export const partTwo = (rows) => {
   return rows.map((row) =>
     parseInt(row, 10),
--- util.js
+++ util.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import * as challenges from './challenges/index.js';
 
 const getRows = (path) => {
-  // check if file exists
+  // Check if file exists
   if (!fs.existsSync(path)) {
     console.log(`[ERROR] File ${path} does not exist!`);
     return [];
@@ -16,13 +16,12 @@ const getRows = (path) => {
 const runChallenge = (challengeNumber) => {
   console.log(`--- Challenge #${challengeNumber} ---\n`);
 
-  // Add 0 to the challenge number if it's less than 10
+  // Add some padding for even spacing
   const challengeFormatted = `${challengeNumber}`.padStart(2, '0');
   const rows = getRows(`${process.cwd()}/inputs/12-${challengeFormatted}.txt`);
 
   const hashPartOne = challenges[`challenge${challengeNumber}`]?.partOne?.(rows);
   const hashPartTow = challenges[`challenge${challengeNumber}`]?.partTwo?.(rows);
-
 
   console.log(`Part One: ${hashPartOne || 'Not implemented'}`);
   console.log(`Part Two: ${hashPartTow || 'Not implemented'}`);
