7f4369e5f53d8be24092e4dc07003d3b4b0e1a4d
Adriano Boccardo Pochettino <adriano.pochettino@linnworks.com>
2023-12-06 10:33:24 +0000

    👕

--- challenges/2023/challenge-06.js
+++ challenges/2023/challenge-06.js
@@ -3,18 +3,16 @@ export const partOne = (rows) => {
   const distances = rows[1].split(' ').filter(x => x !== '').slice(1).map(Number);
 
   let result = 1;
-  for (let x = 0; x < times.length; x++) {
-    const time = times[x];
-    const record = distances[x];
-    let greaters = 0;
-    // run math
-    for (let i = 0; i < time; i++) {
-      const distance = ((time - i) * i);
-      if (distance > record) greaters++;
+  for (let timeIndex = 0; timeIndex < times.length; timeIndex++) {
+    const time = times[timeIndex];
+    const record = distances[timeIndex];
+    let wins = 0;
+    for (let ms = 0; ms < time; ms++) {
+      const distance = ((time - ms) * ms);
+      if (distance > record) wins++;
     }
-    result *= greaters;
+    result *= wins;
   }
-
   return result;
 };
 
@@ -22,12 +20,11 @@ export const partTwo = (rows) => {
   const time = Number(rows[0].split(' ').filter(x => x !== '').slice(1).join(''));
   const record = Number(rows[1].split(' ').filter(x => x !== '').slice(1).join(''));
 
-  let greaters = 0;
-  // run math
-  for (let i = 0; i < time; i++) {
-    const distance = ((time - i) * i);
-    if (distance > record) greaters++;
+  let wins = 0;
+  for (let ms = 0; ms < time; ms++) {
+    const distance = ((time - ms) * ms);
+    if (distance > record) wins++;
   }
 
-  return greaters;
+  return wins;
 };
