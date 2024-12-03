81651ae1d0b4d6f8bdb73ad0d512a8b510acaa97
adriano.boccardo <adriano.boccardo@gmail.com>
2024-12-03 12:14:59 +0000

    feat: day 3

---
+++ challenges/2024/challenge-03.js
@@ -0,0 +1,19 @@
+export const partOne = (_, raw) => {
+  return raw.match(/(mul[(]\d{1,3},\d{1,3}[)])/g).reduce((acc, curr) => {
+    const [first, second] = curr.match(/\d{1,3}/g);
+    return acc + (first * second);
+  }, 0);
+};
+
+export const partTwo = (_, raw) => {
+  return raw.match(/(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))/g)
+    .reduce(({ total, allowed }, curr) => {
+      if (curr === 'do()') return { total, allowed: true };
+      if (curr === 'don\'t()') return { total, allowed: false };
+      if (!allowed) return { total, allowed };
+
+      const [first, second] = curr.match(/\d{1,3}/g);
+      return { total: total + (first * second), allowed }
+
+    }, { total: 0, allowed: true }).total;
+};
--- challenges/2024/index.js
+++ challenges/2024/index.js
@@ -1,7 +1,9 @@
 import * as challenge01 from './challenge-01.js';
 import * as challenge02 from './challenge-02.js';
+import * as challenge03 from './challenge-03.js';
 
 export {
   challenge01,
   challenge02,
+  challenge03,
 };
