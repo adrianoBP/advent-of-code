7e0a96503bc6481d0966b8094408ec3f2880301a
adriano.boccardo <adriano.boccardo@gmail.com>
2024-12-03 12:28:44 +0000

    👕

--- challenges/2024/challenge-03.js
+++ challenges/2024/challenge-03.js
@@ -1,12 +1,12 @@
 export const partOne = (_, raw) => {
-  return raw.match(/(mul[(]\d{1,3},\d{1,3}[)])/g).reduce((acc, curr) => {
+  return raw.match(/mul\(\d{1,3},\d{1,3}\)/g).reduce((acc, curr) => {
     const [first, second] = curr.match(/\d{1,3}/g);
     return acc + (first * second);
   }, 0);
 };
 
 export const partTwo = (_, raw) => {
-  return raw.match(/(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))/g)
+  return raw.match(/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g)
     .reduce(({ total, allowed }, curr) => {
       if (curr === 'do()') return { total, allowed: true };
       if (curr === 'don\'t()') return { total, allowed: false };
